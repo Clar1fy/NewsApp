b3756c033972f2921741158bcc5fe0f29cb8e3f3
Clar1fy <iamonmydoctorpath@gmail.com>
2022-04-10 16:13:29 +0600

    refactor converters

--- app/src/main/java/com/timplifier/newsapp/data/local/room/converters/Converters.kt
+++ app/src/main/java/com/timplifier/newsapp/data/local/room/converters/Converters.kt
@@ -2,6 +2,7 @@ package com.timplifier.newsapp.data.local.room.converters
 
 import androidx.room.TypeConverter
 import com.google.gson.Gson
+import com.timplifier.newsapp.data.remote.dtos.Article
 import com.timplifier.newsapp.data.remote.dtos.Source
 
 class Converters {
@@ -12,4 +13,9 @@ class Converters {
     @TypeConverter
     fun jsonToSource(value: String): Source = Gson().fromJson(value, Source::class.java)
 
+    @TypeConverter
+    fun listToJson(value: List<Article>) = Gson().toJson(value)
+
+    @TypeConverter
+    fun jsonToList(value: String) = Gson().fromJson(value, Array<Article>::class.java).toList()
 }
